a7aa4246fa086521964759f5bff4db8f8f698f25
alexx2021 <78677978+alexx2021@users.noreply.github.com>
2021-11-10 23:37:43 -0800

    Update pickler.py

--- pickler.py
+++ pickler.py
@@ -1,9 +1,24 @@
+import os
+import pickle
+
 class Pickler:
     def __init__(self):
-        pass
+        self.path = os.getcwd()
+
     def save(self, account_manager):
-        pass
+        pickle.dump(account_manager, open("acc.pickle", "wb"))
 
     def load(self):
-        pass
-        #getTest
+        try:
+            file = pickle.load(open( "acc.pickle", "rb" ))
+            return file
+        except FileNotFoundError:
+            print("File does not exist in the CWD")
+    
+    def doesFileExist(self):
+        try:
+            pickle.load(open( "acc.pickle", "rb" ))
+            return True
+        except FileNotFoundError:
+            return False
+        
