5260ae3d4bc62803b1d9885653f79f1767a38508
Thavens <michael.lavery.2017@gmail.com>
2021-11-11 01:20:58 -0800

    0.9.1

--- Runner.py
+++ Runner.py
@@ -1,4 +1,5 @@
 import argparse
+import signal
 from pickler import Pickler
 from AccountManager import AccountManager
 from UserInterface import welcome_screen, login_screen, main_menu, display_schedule, event_manager_screen, add_event_menu, delete_event_menu, register_screen
@@ -7,51 +8,54 @@ from User import User
 class Runner:
     def __init__(self, args):
         self.args = args
-        p = Pickler() #create pickler
-        if p.file_exists:
-            self.am = p.load() #load account manager
+        self.p = Pickler() #create pickler
+        if self.p.file_exists:
+            self.am = self.p.load() #load account manager
         else:
             self.am = AccountManager()
     
     def main_loop(self):
-        id, pwd = self.args.user, self.args.pwd
-        user = None
-        if not self.args.no_gui: #command line logic
-            if self.args.exists: #account exists
-                if not self.am.check_credentials(id, pwd): #account doesn't exist
-                    raise ValueError('no_gui. Wrong id/password!!')
-            else:
-                #make new user
-                user = User(self.args.name,
-                            id,
-                            pwd)
-        else:
-            while True:
-                exists = welcome_screen() #1 for login, 2 for register
-                if exists == 1:
-                    id, pwd = login_screen()
-                    if self.am.check_credentials(id, pwd):
-                        break
-                    continue
-                elif exists == 2:
-                    user = register_screen()
-                    break
+        while True:
+            id, pwd = self.args.user, self.args.pwd
+            user = None
+            if self.args.no_gui: #command line logic
+                if self.args.exists: #account exists
+                    if not self.am.check_credentials(id, pwd): #account doesn't exist
+                        raise ValueError('no_gui. Wrong id/password!!')
                 else:
-                    raise ValueError('welcome screen returned wrong values')
-        
-        if user:
-            self.am.add_user(user)
-        user = self.am.get_user(id, pwd)
-        while item := main_menu() != -1:
-            options = {1 : lambda x : display_schedule(x.schedule),
-                       2 : self.emloop} #general options
-            options[item](user)
-        p.save(self.am)
+                    #make new user
+                    user = User(self.args.name,
+                                id,
+                                pwd)
+            else:
+                while True:
+                    exists = welcome_screen() #1 for login, 2 for register
+                    if exists == 1:
+                        id, pwd = login_screen()
+                        if self.am.check_credentials(id, pwd):
+                            break
+                        continue
+                    elif exists == 2:
+                        user = register_screen()
+                        break
+                    elif exists == -1: #quit
+                        return
+                    else:
+                        raise ValueError('welcome screen returned wrong values')
+            
+            if user:
+                self.am.add_user(user)
+            user = self.am.get_user(id, pwd)
+            while item := main_menu() != -1: #logout then save
+                options = {1 : lambda x : display_schedule(x.schedule), #currently assuming it will block thread when displaying schedule
+                        2 : self.emloop} #general options
+                options[item](user)
+            self.p.save(self.am)
     
     def emloop(user):
         emoptions = {1 : lambda x : x.add_event(add_event_menu()),
-                     2 : lambda x : delete_event_menu(x.schedule)} #event manager options #delete should return event obj not a string?
-        while item := event_manager_screen(user.schedule) != -1:
+                     2 : lambda x : x.delete_event(delete_event_menu(x.schedule))} #event manager options #delete should return event obj not a string?
+        while item := event_manager_screen(user.schedule) != -1: #event manager loop
             emoptions[item]
 
 
