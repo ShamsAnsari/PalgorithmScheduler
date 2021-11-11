362cbc5496533fad4b9e12c2fadfcacf25ba7854
Shams Ansari <shamsahmedansari@gmail.com>
2021-11-10 21:52:35 -0800

    Update AccountManager.py

---
+++ AccountManager.py
@@ -0,0 +1,14 @@
+
+class AccountManager:
+    def __init__(self):
+        self.users = []
+        pass
+
+    def add_user(self, user):
+        pass
+
+    def check_credentials(self, userid, password):
+        return False
+
+    def get_user(self, userid, password):
+        return None
