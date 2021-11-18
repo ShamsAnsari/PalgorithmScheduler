ea4408b35811a7e460e9ac9a62c2e450cb15ba2b
Thavens <michael.lavery.2017@gmail.com>
2021-11-17 21:39:35 -0800

    initial commit

---
+++ userInterface.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+class UserInterface:
+    def __init__(self):
+        self.window = tk.Tk()
+
+    def welcome_screen():
+        '''Allows the user to select to register or '''
