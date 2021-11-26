11ddf0100f022ceadf3f36c88de1bae7a733217d
Ming Senn Teo <71879120+MSTeo01@users.noreply.github.com>
2021-11-26 00:26:01 -0800

    Update user.py

--- user.py
+++ user.py
@@ -1,1 +1,13 @@
+from Schedule import Schedule
 
+class User:
+    def __init__(self, name, userid, password): #Constructor
+        self.name = name
+        self.userid = userid
+        self.password = password
+        self.schedule = Schedule()
+    
+    def __eq__(self, other): #compares two users and returns True if they match
+        return self.userid == other.userid
+
+        
